fc590297eb56b5705c7879bc33cff6ee37629653
Karl Palsson <karlp@tweak.net.au>
2016-03-30 17:13:40 +0000

    adc: f4: update for new naming

--- tests/adc-power/main-stm32f4-disco.c
+++ tests/adc-power/main-stm32f4-disco.c
@@ -22,7 +22,7 @@ int main(void)
 {
 	int i;
 	int j = 0;
-	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_168MHZ]);
+	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 	rcc_periph_clock_enable(RCC_GPIOD);
 	printf("hi guys!\n");
 	/* green led for ticking */
