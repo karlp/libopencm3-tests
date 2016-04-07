835fafe2700930e51532e6ffdf62d049565acd89
Karl Palsson <karlp@tweak.net.au>
2016-04-07 23:26:35 +0000

    squelch errors from netcat when it needs to start oocd
    be nice if I could _keep_ errors when V=1 but....

--- rules.mk
+++ rules.mk
@@ -152,13 +152,13 @@ $(PROJECT).elf: $(OBJS) $(LDSCRIPT)
 %.flash: %.elf
 	@printf "  FLASH\t$<\n"
 ifeq (,$(OOCD_FILE))
-	$(Q)(echo "halt; program $(*).elf verify reset" | nc -4 localhost 4444 ) || \
+	$(Q)(echo "halt; program $(*).elf verify reset" | nc -4 localhost 4444 2>/dev/null) || \
 		$(OOCD) -f interface/$(OOCD_INTERFACE).cfg \
 		-f target/$(OOCD_TARGET).cfg \
 		-c "program $(*).elf verify reset exit" \
 		$(NULL)
 else
-	$(Q)(echo "halt; program $(*).elf verify reset" | nc -4 localhost 4444 ) || \
+	$(Q)(echo "halt; program $(*).elf verify reset" | nc -4 localhost 4444 2>/dev/null) || \
 		$(OOCD) -f $(OOCD_FILE) \
 		-c "program $(*).elf verify reset exit" \
 		$(NULL)
