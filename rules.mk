117e157bd80bb5e76c5ffd7408c90c611658629a
Karl Palsson <karlp@tweak.net.au>
2018-09-15 15:40:19 +0000

    rules.mk: turn unused variable warnings back on

--- rules.mk
+++ rules.mk
@@ -85,7 +85,7 @@ TGT_CFLAGS += $(OPT) $(CSTD) -ggdb3
 TGT_CFLAGS += $(ARCH_FLAGS)
 TGT_CFLAGS += -fno-common
 TGT_CFLAGS += -ffunction-sections -fdata-sections
-TGT_CFLAGS += -Wextra -Wshadow -Wno-unused-variable -Wimplicit-function-declaration
+TGT_CFLAGS += -Wextra -Wshadow -Wimplicit-function-declaration
 TGT_CFLAGS += -Wredundant-decls -Wstrict-prototypes -Wmissing-prototypes
 
 TGT_CXXFLAGS += $(OPT) $(CXXSTD) -ggdb3
