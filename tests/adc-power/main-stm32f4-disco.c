0ae3917f1f372baef263370282919493016d3fa4
Karl Palsson <karlp@etactica.com>
2019-08-26 16:54:40 +0000

    adc-power: update apis

--- tests/adc-power/main-stm32f4-disco.c
+++ tests/adc-power/main-stm32f4-disco.c
@@ -21,7 +21,7 @@
 int main(void)
 {
 	int i;
-	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+	rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 	rcc_periph_clock_enable(RCC_GPIOD);
 	printf("hi guys!\n");
 	/* green led for ticking */
